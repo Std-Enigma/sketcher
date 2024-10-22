fcecb4e6181aeb8bc62bc166ff1dd775b0ff6fed
Std-Enigma <std.ilia.bahrebar@gmail.com>
2024-10-22 14:11:15 +0330

    style: format main.py

--- main.py
+++ main.py
@@ -1,28 +1,33 @@
-
-from turtle import Turtle, Screen
+from turtle import Screen, Turtle
 
 turtle = Turtle()
 screen = Screen()
 turtle.color("white")
 screen.bgcolor("black")
 
+
 def go_forward():
     turtle.forward(10)
+
 
 def go_backward():
     turtle.back(10)
 
+
 def turn_right():
     turtle.right(5)
 
+
 def turn_left():
     turtle.left(5)
+
 
 def reset():
     turtle.clear()
     turtle.penup()
     turtle.home()
     turtle.pendown()
+
 
 screen.listen()
 screen.onkeypress(go_forward, "w")
